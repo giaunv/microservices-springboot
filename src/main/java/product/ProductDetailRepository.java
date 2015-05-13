d6535c2d313869cc527f5096260913c011e4e0bf
Giau Ngo <giaunv@gmail.com>
2015-05-13 15:05:13 +0700

    Search

--- src/main/java/product/ProductDetailRepository.java
+++ src/main/java/product/ProductDetailRepository.java
@@ -1,9 +1,16 @@
 package product;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProductDetailRepository extends PagingAndSortingRepository<ProductDetail, String> {
     ProductDetail findByInventoryId(String inventoryId);
+
+    @Query("select p from ProductDetail p where UPPER(p.productName) like UPPER(?1) or " +
+            "UPPER(p.longDescription) like UPPER(?1)")
+    List search(String term);
 }
